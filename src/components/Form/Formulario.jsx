244bb12249b74036924a038efb2ff40c0aa5f356
santiago <contatofasantiago@hotmail.com>
2023-10-20 14:49:01 -0300

    beta - não deixa o IP repetir quando já registrado

--- src/components/Form/Formulario.jsx
+++ src/components/Form/Formulario.jsx
@@ -1,72 +1,83 @@
 import React, { useState } from "react";
-import '../css/homePage.css'
 
 export default function Formulario() {
 
-    const [ipRegistroMaquina, setipRegistroMaquina] = useState([])
-    const [ipValue, setIpValue] = useState()
+    const [ipRegistroMaquina, setIpRegistroMaquina] = useState([]);
+    const [ipValue, setIpValue] = useState();
+    const [armazemHora, setArmazemHora] = useState([]);
 
-    const [armazemHora, setArmazemHora] = useState([])
-    
-    // escopo destinado em formatar o IP, permitindo seu envio ou não
-    const formatIp = (value) => {
-        const cleanCharacter = value.replace(/\D/g, '');
-        const addFormatValue = cleanCharacter.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3.$4');
-        return addFormatValue;
+    // escopo destinado a formatar o IP 
+    const formatIp = (ip) => {
+        const cleanCharacter = ip.replace(/\D/g, '');
+        const ipPattern = cleanCharacter.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3.$4');
+        return ipPattern;
+    };
+
+    // verifica se o IP já foi inserido
+    const ipJaRegistrado = (toCheckIp) => {
+        return ipRegistroMaquina.some((ipRegistro) => ipRegistro === toCheckIp);
     }
-    
     // responsável para registrar o IP
     function hundleSubmit(e) {
-        e.preventDefault()
+        e.preventDefault();
         if (ipValue === '') {
-            alert('Campo vazio, tente novamente.')  
+            alert('Campo vazio, tente novamente.');
             return;
         }
         // verifica se o IP é válido            
-        const novoRegistro = ipValue
+        const novoRegistro = ipValue;
         if (novoRegistro.length < 12) {
-            alert('Não foi possível identificar este IP, por favor, tente novamente.') 
+            alert('Não foi possível identificar este IP, por favor, tente novamente.');
             return;
         }
         // registra o IP no estado
-        setipRegistroMaquina([...ipRegistroMaquina, novoRegistro]) 
-        setIpValue('')
+        setIpRegistroMaquina([...ipRegistroMaquina, novoRegistro]);
+        setIpValue('');
         gravarHoraRegistro();
+
+        if (!ipJaRegistrado(novoRegistro)) {
+            return true;
+        } else {
+            alert('Esse IP já foi inserido ou validado por um analista.');
+            ipValue('');
+            return false;
+        }
     }
+
     // responsável por formatar o IP
     const hundleInputChange = (eventFormat) => {
         const toApplyFormated = formatIp(eventFormat.target.value);
-        setIpValue(toApplyFormated)
-    }
+        setIpValue(toApplyFormated);
+    };
 
     // responsável por gravar a hora de registro do último IP inserido
     function gravarHoraRegistro() {
-        const corretaDataParaRegistro = new Date()
-        setArmazemHora([...armazemHora, corretaDataParaRegistro])
+        const corretaDataParaRegistro = new Date();
+        setArmazemHora([...armazemHora, corretaDataParaRegistro]);
     }
 
     return (
         <div>
             <main>
-                <form onSubmit={hundleSubmit} >
+                <form onSubmit={hundleSubmit}>
                     <input
+                        id="inputIp"
                         type="text"
                         value={ipValue}
                         onChange={hundleInputChange}
-                        placeholder = "Digite o IP"
-                        maxLength={12}
-                    />
+                        placeholder="Digite o IP"
+                        maxLength={12} />
                     <button>Registrar</button>
                 </form>
+                <section>
+                    <h5>Últimos registros</h5>
+                    <li className='styleList'>
+                        {ipRegistroMaquina.map((item, index) => (
+                            <li key={index} className="lineIndex"> ({index + 1}) - {item} último registro {armazemHora[index].toLocaleString()} </li>
+                        ))}
+                    </li>
+                </section>
             </main>
-            <section>
-                <h5>Últimos registros</h5>
-                <li className='styleList'>
-                    {ipRegistroMaquina.map((item, index) => (
-                        <li key={index} > {index + 1} {item} último registro {armazemHora[index].toLocaleString()} </li> 
-                    ))}
-                </li>
-            </section>
         </div>
-    )
-}  
+    );
+}
