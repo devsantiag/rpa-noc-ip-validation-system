e7e59ca2ac87d0f482e74ffeef0b0163037f05c5
santiago <contatofasantiago@hotmail.com>
2023-11-11 09:46:42 -0300

    renomeado arquivo

---
+++ src/components/home/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../css/homePage.css'
+import Formulario from "../Form/Formulario";
+
+
+
+// home pagepage main
+export default function HomePage() {
+    return(
+        <div>
+            <Formulario/>
+        </div>
+    )
+}
