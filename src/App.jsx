c2b5d764863b9f9ee36684eace8830b802e1986d
santiago <contatofasantiago@hotmail.com>
2023-11-11 12:32:40 -0300

    adicionado titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Formulario from '../src/Form/Formulario'
+import Footer from './components/Footer/Footer'
 
 export default function App() {
-    return(
+    return (
         <div>
-            <Formulario/>
+            <header>
+                <h3>Registros de RPA</h3>
+            </header>
+            <Formulario />
+            <Footer />
         </div>
     )
 }
